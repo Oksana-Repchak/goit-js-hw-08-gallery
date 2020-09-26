84f9b81330bd5a2c0f73f6ef6d9fc609ee8215b7
Oksana Repchak <oksa.repchak@gmail.com>
2020-09-27 00:17:02 +0300

    modal

--- js/create-gallery.js
+++ js/create-gallery.js
@@ -1,9 +1,16 @@
 import galleryReference from './gallery-items.js';
 
 const galleryList = document.querySelector('.js-gallery');
-const galleryMarkup = createGalleryMarkup(galleryReference);
+const CloseModalBtn = document.querySelector('[data-action="close-lightbox"]');
+const imageEl = document.querySelector('.lightbox__image');
+const lightboxOverlay = document.querySelector('.lightbox__overlay');
 
+const galleryMarkup = createGalleryMarkup(galleryReference);
 galleryList.insertAdjacentHTML('afterbegin', galleryMarkup);
+
+galleryList.addEventListener('click', onGalleryListClick);
+CloseModalBtn.addEventListener('click', onCloseModal);
+lightboxOverlay.addEventListener('click', onOverlayClick);
 
 function createGalleryMarkup(items) {
   return items
@@ -20,3 +27,62 @@ function createGalleryMarkup(items) {
     })
     .join('');
 }
+
+function onGalleryListClick(event) {
+  event.preventDefault();
+  const isGalleryLink = event.target.classList.contains('gallery__image');
+  if (!isGalleryLink) {
+    return;
+  }
+  const modalWindowEl = document.querySelector('.lightbox');
+
+  onCloseModal();
+  onOpenModal(modalWindowEl);
+  setAttributeValue(event.target.dataset.source);
+}
+
+function setAttributeValue(srs) {
+  imageEl.src = srs;
+}
+
+function cleanAttributeValue(src) {
+  imageEl.src = '';
+}
+
+function onCloseModal(event) {
+  window.removeEventListener('keydown', onEscKeyDown);
+  const curentActiveImage = document.querySelector('.lightbox.is-open');
+
+  if (curentActiveImage) {
+    curentActiveImage.classList.remove('is-open');
+  }
+  cleanAttributeValue(event);
+}
+
+function onOpenModal(modalEl) {
+  window.addEventListener('keydown', onEscKeyDown);
+  // window.addEventListener('keydown', onPointerKeyDown);
+
+  modalEl.classList.add('is-open');
+}
+
+function onOverlayClick(event) {
+  if (event.currentTarget === event.target) {
+    onCloseModal();
+  }
+}
+
+function onEscKeyDown(event) {
+  if (event.code === 'Escape') {
+    onCloseModal();
+  }
+}
+
+// function onPointerKeyDown(event) {
+//   if (event.code === 'ArrowRight') {
+    // event.target = event.target.parentNode.nextSibling;
+    // console.log(event.target.parentNode.nextSibling);
+  // }
+  //     if (event.code === 'ArrowLeft') {
+  // }
+// }
